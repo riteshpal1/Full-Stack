2a3045c8088f47ade9808afa07bc98e887d7bc76
riteshpal1 <104260480+riteshpal1@users.noreply.github.com>
2024-03-23 12:27:50 +0530

    2

--- advanced.js
+++ advanced.js
@@ -3,6 +3,7 @@
 ///- var function scoped hota hai
 /// - var function scoped hota hai => var apne parent fnc mein kahi bhi use ho sakta hai 
 ///- var adds itself to the window object
+
 function abcd(){
   for(var i = 1; i<12; i++){
     console.log(i);
@@ -18,7 +19,6 @@ function abcd(){
 // let braces scopred hota hau .
 // let braces scoped hota hai.
 
-
 // js language me kuch cheezen nahi hai jo hum use kar sakte haiu aur wo cheeje hume 
 // js se nhi balki browser se milti hai, aise saare features jo js ka part nahi hai but
 // fir bhi use kar sakte hai unhe hum dhoondh sakte hai ek particular object me jiska naam hai window.  
@@ -26,4 +26,5 @@ function abcd(){
 // or js main kai saare features hai pr kuch features jo hum use karte hai , wo features wo nahi hai
 // par fir bhi use kar paate hai kyuki wo features js language use kar leti hai window self, 
 // aur window hai ek box of features given by browser to use with js
+
 
