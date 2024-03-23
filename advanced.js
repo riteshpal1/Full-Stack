55ccc38e048a9cfb620b5529f390f8f090bd7dea
riteshpal1 <104260480+riteshpal1@users.noreply.github.com>
2024-03-23 11:47:29 +0530

    first commit

---
+++ advanced.js
@@ -0,0 +1,28 @@
+// Difference b/w var let const
+// - var old js me tha
+///- var function scoped hota hai
+/// - var function scoped hota hai => var apne parent fnc mein kahi bhi use ho sakta hai 
+///- var adds itself to the window object
+function abcd(){
+  for(var i = 1; i<12; i++){
+    console.log(i);
+  }
+  console.log(i);
+}
+
+// - let const new js me hai
+// let braces scopedd hota hai
+// let const doesn't adds 
+
+// let braces scoped hota hai 
+// let braces scopred hota hau .
+// let braces scoped hota hai.
+
+
+// js language me kuch cheezen nahi hai jo hum use kar sakte haiu aur wo cheeje hume 
+// js se nhi balki browser se milti hai, aise saare features jo js ka part nahi hai but
+// fir bhi use kar sakte hai unhe hum dhoondh sakte hai ek particular object me jiska naam hai window.  /or
+js main kai saare features hai pr kuch features jo hum use karte hai , wo features wo nahi hai par fir bhi 
+use kar paate hai kyuki wo features js language use kar leti hai window self, aur window hai ek box of features 
+given by browser to use with js
+
---
+++ script.js
@@ -0,0 +1,115 @@
+// word vs keyword
+
+// harsh word
+//  for keyword   
+
+//examples of work and keyword
+
+
+#keywords  
+//for, is, if, else
+
+#variables&constants
+
+//variable is that where we staore the Data 
+//constants are that which are never changes
+
+#hoisting
+// variables= and fuctions are hoisted which means there declaration is moved on the top of code.
+
+#types in JS
+//They are two types
+//reference = {} [] () 
+ 
+//aisi koi bhi value jisko copy karne pr real copy nahi hota, 
+//balki us main value ka reference pass hojaata hai, 
+//use hum refrence value kehte hai, aur jisko copy karne par 
+//real copy  ho jaaye wo value primitive type value hoti hai
+
+var a = [12,13]
+var b = a;
+
+
+#conditionals 
+// if-else,else-if
+if(11>12){
+
+}
+else if(12>13){
+
+}
+else if(14>16){
+
+}
+else{
+
+}
+
+#loops - means-repeat
+//example 3 time print name
+console.log("aman");
+console.log("aman");
+console.log("aman");
+
+for(Start;end;change){
+
+}
+
+for(var i = 0; i<11; i++){
+  console.log(i);
+}
+
+
+#functions
+// 1 jab aapka code aap turant chalaana nhi chahte future me chalana chahte ho.
+// 2 jab aap code reuse krna chahte ho
+// 3 jab aap code chalana chahte ho har baar with different data
+
+function abcd(){
+    //function statements
+}
+
+function helobolo(){
+  console.log("hello");
+}
+helobolo();
+
+function (a,b,c){
+  console.log(a,b,c);
+}
+abc(12,13,14);
+
+//argument = real value jo hum dete hai fun chalate waqt
+//parameter = variables jinme value store hoti hai arguments waali
+
+
+#arrays = hum ek variable me ek value store kar paae hai par jab 
+//hamein ek se jyada value store karni ho tab fir use hota hai array ka, 
+//matlab ki array aapko freedom deta hai ek se jyada value use karne ki
+
+// array = group of values  
+
+var arr = [1,2,3,4,5] values start from 0 index 
+
+// push pop shift unshift
+
+arr.slice(2,1)
+
+
+#objects - ek se jyada bande ki baat ki to hua Array, ek bande ke baare me saari baat ki to hua object 
+
+// object hai ek bande ki detail ko hold karna, in a key value pair
+
+// 1) blank object
+var a= {}
+
+// 2) filled object
+var a = {
+  age: 24,
+  name: "Harsh"
+  email: anything@gmail.com
+  linkedin: "mohit"
+  contact: t55778u213t612u3u
+}
+
+
