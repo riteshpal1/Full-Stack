bcd61694ed85c0e4dc76e592f53fb3bff8c35ce7
riteshpal1 <104260480+riteshpal1@users.noreply.github.com>
2024-03-23 12:15:40 +0530

    1

--- advanced.js
+++ advanced.js
@@ -21,8 +21,9 @@ function abcd(){
 
 // js language me kuch cheezen nahi hai jo hum use kar sakte haiu aur wo cheeje hume 
 // js se nhi balki browser se milti hai, aise saare features jo js ka part nahi hai but
-// fir bhi use kar sakte hai unhe hum dhoondh sakte hai ek particular object me jiska naam hai window.  /or
-js main kai saare features hai pr kuch features jo hum use karte hai , wo features wo nahi hai par fir bhi 
-use kar paate hai kyuki wo features js language use kar leti hai window self, aur window hai ek box of features 
-given by browser to use with js
+// fir bhi use kar sakte hai unhe hum dhoondh sakte hai ek particular object me jiska naam hai window.  
+
+// or js main kai saare features hai pr kuch features jo hum use karte hai , wo features wo nahi hai
+// par fir bhi use kar paate hai kyuki wo features js language use kar leti hai window self, 
+// aur window hai ek box of features given by browser to use with js
 
